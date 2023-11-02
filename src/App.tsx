310ae53a1c39b299fc7d9ae1c324e1701ae87fdb
PVSKR <skr.polu@hotmail.com>
2023-11-02 20:52:07 +0530

    Navbar.tsx implementing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
+import Navbar from "./scenes/navbar";
+
 function App() {
   return (
     <>
-      <div>Hi...</div>
+      <div className="app  bg-gray-20">
+        <Navbar />
+      </div>
     </>
   );
 }
---
+++ src/scenes/navbar/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type Props = {
+  //   selectedPage: SelectedPage;
+  //   isTopOfPage: boolean;
+  //   setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Navbar = ({}: Props) => {
+  return (
+    <>
+      <div>Pomoto</div>
+    </>
+  );
+};
+
+export default Navbar;
