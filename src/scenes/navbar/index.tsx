dd3da0ea907ce48d116706d54a3fadb19d4870b4
PVSKR <skr.polu@hotmail.com>
2023-11-03 14:02:49 +0530

    navbar items placed.

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,4 +1,8 @@
-import React from "react";
+import { motion } from "framer-motion";
+import HText from "../../shared/HText";
+import setting from "../../assets/setting.svg";
+import report from "../../assets/report.svg";
+import login from "../../assets/login.svg";
 
 type Props = {
   //   selectedPage: SelectedPage;
@@ -9,7 +13,35 @@ type Props = {
 const Navbar = ({}: Props) => {
   return (
     <>
-      <div>Pomoto</div>
+      <motion.div
+        className=" flex mx-auto w-5/6 ml-[-.1rem]"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 1.5 }}
+        variants={{
+          hidden: { opacity: 0, x: -50 },
+          visible: { opacity: 1, x: 0 },
+        }}>
+        <HText>
+          <span className="pl-2">Pomoto</span>
+        </HText>
+        <div className="flex  justify-between">
+          <div className="flex flex-row justify-center m-2 items-center cursor-pointer ">
+            <img src={report} alt="" width={20} height={20} />
+            <h2>Report</h2>
+          </div>
+          <div className="flex flex-row  items-center  m-2 cursor-pointer">
+            <img src={setting} alt="" width={20} height={20} />
+            <h2>Setting</h2>
+          </div>
+
+          <div className="flex flex-row justify-center m-2 items-center cursor-pointer ">
+            <img src={login} alt="" width={20} height={20} />
+            <h2>Login</h2>
+          </div>
+        </div>
+      </motion.div>
     </>
   );
 };
