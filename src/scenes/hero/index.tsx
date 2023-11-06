b798bf597fb3e7a40f461e7d7221aea4c6c638d7
PVSKR <skr.polu@hotmail.com>
2023-11-06 10:40:53 +0530

    The 'reset' and 'next' buttons have been modified to support a mobile layout and have been styled accordingly.

--- src/scenes/hero/index.tsx
+++ src/scenes/hero/index.tsx
@@ -1,57 +1,63 @@
+import React from "react";
 import { SelectedPage } from "@/shared/types";
-
 import Link from "../navbar/Link";
 import reset from "@/assets/reset.svg";
 import next from "@/assets/next.svg";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 type Props = {
   selectedPage: SelectedPage;
-
   setSelectedPage: (value: SelectedPage) => void;
 };
 
 const Hero = ({ selectedPage, setSelectedPage }: Props) => {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
   return (
-    <>
-      <div className=" h-screen flex items-center justify-center p-2">
-        <div className=" w-3/6  flex items-center justify-center h-4/6  bg-gray-950 rounded text-gray-100 relative ">
-          <div className="absolute w-full h-full flex flex-col items-center justify-around ">
-            <div className="flex flex-row m-2 ">
-              <button className=" m-2 px-2 border-2 rounded xs:mx-1 xs:text-xs">
-                <Link
-                  page="Pomodoro"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-              </button>
-              <button className="m-2 px-2 border-2 rounded xs:mx-1 xs:text-xs ">
-                <Link
-                  page="ShortBreak"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-              </button>
-              <button className="m-2 px-2 border-2 rounded xs:mx-1 xs:text-xs">
-                <Link
-                  page="LongBreak"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-              </button>
-            </div>
-            <div className="flex flex-row m-2  items-center">
-              <button className=" m-2 px-2 border-2 rounded xs:mx-1 xs:text-xs">
+    <div className="h-screen flex items-center justify-center p-2">
+      <div
+        className={`flex items-center justify-center h-${
+          isAboveMediumScreens ? "3/6" : "4/6"
+        } w-${
+          isAboveMediumScreens ? "3/6" : "full"
+        }  bg-gray-950 rounded text-gray-100 relative  `}>
+        <div className="w-full h-full flex flex-col items-center justify-around">
+          <div className="flex flex-row m-2">
+            <button className="m-2 px-2 border-2 rounded xs:mx-1 xs:text-xs">
+              <Link
+                page="Pomodoro"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+            </button>
+            <button className="m-2 px-2 border-2 rounded xs:mx-1 xs:text-xs">
+              <Link
+                page="ShortBreak"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+            </button>
+            <button className="m-2 px-2 border-2 rounded xs:mx-1 xs:text-xs">
+              <Link
+                page="LongBreak"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+            </button>
+          </div>
+          {isAboveMediumScreens ? (
+            <div className="flex flex-row m-2 items-center gap-4">
+              <button className="m-2 px-2 border-2 rounded ">
                 <Link
                   page="reset"
                   selectedPage={selectedPage}
                   setSelectedPage={setSelectedPage}
                 />
               </button>
-              <div className="w-40 z-1 h-40 bg-white rounded-full text-blue-400 flex items-center justify-center">
-                {" "}
+              <div className="w-28 z-1 h-28 bg-white rounded-full text-blue-400 flex items-center justify-center">
                 25:00
               </div>
-              <button className=" m-2 px-2 border-2 rounded xs:mx-1 xs:text-xs">
+              <button className="m-2 px-2 border-2 rounded ">
                 <Link
                   page="Next"
                   selectedPage={selectedPage}
@@ -59,10 +65,22 @@ const Hero = ({ selectedPage, setSelectedPage }: Props) => {
                 />
               </button>
             </div>
-          </div>
+          ) : (
+            <div className="flex flex-row justify-between items-center gap-1">
+              <button className="flex bg-white m-2  border-2 rounded w-12 h-12 items-center  justify-center">
+                <img className="  " src={reset} alt="" width={24} height={24} />
+              </button>
+              <div className="w-28 z-1 h-28 bg-white rounded-full text-blue-400 flex items-center justify-center">
+                25:00
+              </div>
+              <button className="flex bg-white m-2  border-2 rounded w-12 h-12 items-center  justify-center">
+                <img className="  " src={next} alt="" width={24} height={24} />
+              </button>
+            </div>
+          )}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
