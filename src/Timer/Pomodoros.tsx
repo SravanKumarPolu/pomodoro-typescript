057e96e06fdbd60f32e90c76f113c57a2bff4d91
PVSKR <skr.polu@hotmail.com>
2023-11-24 01:45:49 +0530

    setting inside pomodoro timer value  not mapping to  work inside pomodoro timer value

--- src/Timer/Pomodoros.tsx
+++ src/Timer/Pomodoros.tsx
@@ -1,40 +1,69 @@
-// Pomodoros.tsx
-import React, { useState } from "react";
 import Pomodoro from "@/pomodoro/TimerComponents";
-import useMediaQuery from "@/hooks/useMediaQuery";
+import { SelectedPage } from "@/shared/types";
+import { useEffect, useState } from "react";
 
-type Props = {};
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
 
-const Pomodoros: React.FC<Props> = () => {
-  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  selectedPage?: SelectedPage;
+  setShortBreakTime: (newTime: number) => void;
+};
+
+const Pomodoros = ({
+  setSelectedPage,
+  setShortBreakTime,
+  selectedPage,
+}: Props) => {
+  const [newTime, setNewTime] = useState("");
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
-  const [minutes, setMinutes] = useState(25);
-  const [seconds, setSeconds] = useState(0);
 
-  const toggleTimer = () => {
-    setIsActive(!isActive);
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+
+    if (isActive && time > 0) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime - 1);
+      }, 1000);
+    } else if (time === 0) {
+      setIsActive(false);
+      setTime(25 * 60);
+
+      setSelectedPage(selectedPage || SelectedPage.Pomodoro);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, time, setSelectedPage, selectedPage]);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const seconds = parseInt(newTime) * 60;
+    if (!isNaN(seconds) && seconds > 0) {
+      setShortBreakTime(seconds);
+    }
+
+    setNewTime("");
   };
-
-  const handleMinuteChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newMinutes = parseInt(e.target.value, 10);
-    setMinutes(isNaN(newMinutes) ? 0 : newMinutes);
-  };
-
-  const handleSecondChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newSeconds = parseInt(e.target.value, 10);
-    setSeconds(isNaN(newSeconds) ? 0 : newSeconds);
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTime(e.target.value);
   };
 
   return (
     <div>
-      <div>
-        <h2 className="text-gray-400 font-weight-500">Pomodoro</h2>
-        <div>
-          <input type="number" value={minutes} onChange={handleMinuteChange} />
-        </div>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <h2 className="text-gray-400 font-weight-500">Short Break</h2>
+        <input
+          className="w-[6rem] h-[2rem] bg-gray-200 rounded-md p-1"
+          type="number"
+          placeholder="Enter minutes"
+          defaultValue={time / 60}
+          onChange={handleInputChange}
+        />
+        <button type="submit"></button>
+      </form>
     </div>
   );
 };
 
-export default Pomodoros;
+export default Pomodoro;
