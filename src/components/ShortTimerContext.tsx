8f4cda3aa893731c8a2733c45400e4e1129dfd02
PVSKR <skr.polu@hotmail.com>
2023-11-26 03:25:32 +0530

    shortTimerContext.tsx file reacted

---
+++ src/components/ShortTimerContext.tsx
@@ -0,0 +1,46 @@
+import { type } from "os";
+import React, {
+  createContext,
+  useContext,
+  ReactNode,
+  useState,
+  Dispatch,
+  SetStateAction,
+} from "react";
+
+interface TimerContextProps {
+  timerValue: number;
+  handleTimerChange: Dispatch<SetStateAction<number>>;
+}
+
+const TimerContext = createContext<TimerContextProps | undefined>(undefined);
+
+interface TimerProviderProps {
+  children: ReactNode;
+}
+export const TimerProvider: React.FC<TimerProviderProps> = ({ children }) => {
+  const [timerValue, setTimerValue] = useState<number>(5);
+  const handleTimerChange: TimerContextProps["handleTimerChange"] = (
+    newValue
+  ) => {
+    setTimerValue((prevTimerValue) =>
+      typeof newValue === "function" ? newValue(prevTimerValue) : newValue
+    );
+  };
+
+  return (
+    <TimerContext.Provider value={{ timerValue, handleTimerChange }}>
+      {children}
+    </TimerContext.Provider>
+  );
+};
+
+export const useTimerContext = (): TimerContextProps => {
+  const context = useContext(TimerContext);
+
+  if (!context) {
+    throw new Error("useTimerContext must be used within a TimerProvider");
+  }
+
+  return context;
+};
