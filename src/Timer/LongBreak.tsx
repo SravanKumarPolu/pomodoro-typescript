cf247ffe8b3881e6a90f4f89f6ef950233a44634
PVSKR <skr.polu@hotmail.com>
2023-11-26 03:58:29 +0530

    Timer folder inside  LongBreak.tsx file modified to support TimerContext.tsx

--- src/Timer/LongBreak.tsx
+++ src/Timer/LongBreak.tsx
@@ -1,49 +1,30 @@
+import { useTimerContext } from "@/components/TimerContext";
 import { SelectedPage } from "@/shared/types";
-import React, { useEffect, useState } from "react";
 
 type Props = {
-  setLongBreakTime: (newTime: number) => void;
   setSelectedPage: (value: SelectedPage) => void;
 
   selectedPage?: SelectedPage;
+  setShortBreakTime: (newTime: number) => void;
 };
 
-const LongBreak = ({
-  setLongBreakTime,
-  setSelectedPage,
-  selectedPage,
-}: Props) => {
-  const [newTime, setNewTime] = useState("");
-  const [time, setTime] = useState(15 * 60);
-  const [isActive, setIsActive] = useState(false);
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    const seconds = parseInt(newTime) * 60;
-    if (!isNaN(seconds) && seconds > 0) {
-      setLongBreakTime(seconds);
-    }
-
-    setNewTime("");
-  };
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNewTime(e.target.value);
-  };
+const LongBreak = ({}: Props) => {
+  const { timerValue3, handleTimerChange3 } = useTimerContext();
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <h2 className="text-gray-400 font-weight-500">Long Break</h2>
+      <div className="mb-4">
+        <label htmlFor="timerValue" className="text-gray-400 font-weight-500">
+          Long Break
+        </label>
         <input
-          className="w-[6rem] h-[2rem] bg-gray-200 rounded-md p-1"
           type="number"
-          placeholder="Enter minutes"
-          defaultValue={time / 60}
-          onChange={handleInputChange}
+          id="timerValue"
+          value={timerValue3}
+          onChange={(e) => handleTimerChange3(parseInt(e.target.value))}
+          className="w-[6rem] h-[2rem] bg-gray-200 rounded-md p-1"
         />
-        <button type="submit"></button>
-      </form>
+      </div>
     </div>
   );
 };
