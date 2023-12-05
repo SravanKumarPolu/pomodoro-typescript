fb141cafe112673c8047e4511265bf51c8af4b39
PVSKR <skr.polu@hotmail.com>
2023-12-05 13:21:10 +0530

    theme modification goinon

--- src/Theme/index.tsx
+++ src/Theme/index.tsx
@@ -1,9 +1,14 @@
+import ThemeSvg from "@/assets/theme.svg";
 type Props = {};
 
 const Themes = ({}: Props) => {
   return (
-    <div className="w-[18rem] p-2">
-      <h2>Theme</h2>
+    <div className="flex flex-col w-[20rem] p-2 border-b-2 border-white-500">
+      <div className="flex flex-row p-1 gap-1">
+        <img src={ThemeSvg} width={15} height={15} />
+
+        <h2>Theme</h2>
+      </div>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </div>
   );
