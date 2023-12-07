5aeba2f498d184aaa8766c8a400a1da7c25c908d
PVSKR <skr.polu@hotmail.com>
2023-12-07 16:00:36 +0530

    Theme color popup creation .

--- src/Theme/index.tsx
+++ src/Theme/index.tsx
@@ -3,8 +3,42 @@ import ThemeSvg from "@/assets/theme.svg";
 
 type Props = {};
 
+const Popup = ({
+  isOpen,
+  onClose,
+  children,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}) => {
+  return (
+    <>
+      {isOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
+          <div className="relative w-auto max-w-3xl mx-auto my-6">
+            <div className="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none">
+              <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                <button
+                  className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                  onClick={onClose}>
+                  <span className="text-red h-6 w-6 text-2xl block outline-none focus:outline-none">
+                    ×
+                  </span>
+                </button>
+              </div>
+              <div className="relative p-6 flex-auto">{children}</div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
 const Themes = ({}: Props) => {
   const [showAllColors, setShowAllColors] = useState(false);
+  const [isPopupOpen, setPopupOpen] = useState(false);
 
   const colorButtons = [
     "bg-red-400",
@@ -28,6 +62,14 @@ const Themes = ({}: Props) => {
 
   const displayedColors = showAllColors ? allColorButtons : colorButtons;
 
+  const openPopup = () => {
+    setPopupOpen(true);
+  };
+
+  const closePopup = () => {
+    setPopupOpen(false);
+  };
+
   return (
     <div className="flex flex-col w-[20rem] p-2 border-b-2 border-white-500">
       <div className="flex flex-row p-1 gap-1">
@@ -39,19 +81,25 @@ const Themes = ({}: Props) => {
           <h3>Color Themes</h3>
           <div className="flex w-36  flex-wrap flex-row gap-1">
             {displayedColors.map((color, index) => (
-              <button key={index} className={`w-5 h-5  rounded ${color}`} />
-            ))}
-            {!showAllColors && (
               <button
-                className="w-5 h-5 rounded bg-gray-300"
-                onClick={() => setShowAllColors(true)}>
-                +
-              </button>
-            )}
+                key={index}
+                className={`w-5 h-5  rounded ${color}`}
+                onClick={openPopup}
+              />
+            ))}
           </div>
         </div>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
       </div>
+
+      <Popup isOpen={isPopupOpen} onClose={closePopup}>
+        {/* Content of your popup goes here */}
+        <div className="flex w-36  flex-wrap flex-row gap-1">
+          {allColorButtons.map((color, index) => (
+            <button key={index} className={`w-5 h-5  rounded ${color}`} />
+          ))}
+        </div>
+      </Popup>
     </div>
   );
 };
