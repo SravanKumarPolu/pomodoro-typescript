ee9c3f8cc2baf7360c800d4a46fffaa6441aa615
PVSKR <skr.polu@hotmail.com>
2023-12-08 23:20:21 +0530

    hero page modified to support mobile layout of  iPhone SE.

--- src/scenes/hero/index.tsx
+++ src/scenes/hero/index.tsx
@@ -78,10 +78,10 @@ const Hero: React.FC<Props> = ({
         className={`flex h-[36rem] mt-0 flex-col items-center justify-center pt-2 p-10 ${
           isAboveMediumScreens ? "w-5/6" : "w-full"
         } bg-pink-500 rounded-sm text-white absolute`}>
-        <div className="flex flex-col items-center justify-center p-2 bg-gradient-to-b from-pink-600 via-pink-500 to-pink-400">
+        <div className="flex  flex-col items-center justify-center p-2 bg-gradient-to-b from-pink-600 via-pink-500 to-pink-400">
           <div className="flex flex-row m-2 ">
             <button
-              className="m-2 p-1 bg-white bg-opacity-50 rounded xs:mx-1 xs:text-xs active:bg-slate-400 focus-within:bg-slate-400 hover:bg-slate-400 "
+              className="m-1 xs:m-2 xs:p-1 bg-white bg-opacity-50 rounded xs:mx-1 xs:text-xs active:bg-slate-400 focus-within:bg-slate-400 hover:bg-slate-400 "
               onClick={() => setSelectedPage(SelectedPage.Pomodoro)}>
               <Link
                 page="Pomodoro"
@@ -90,7 +90,7 @@ const Hero: React.FC<Props> = ({
               />
             </button>
             <button
-              className="my-2 py-1 bg-white bg-opacity-50  rounded xs:mx-1 xs:text-xs focus-within:bg-slate-400 hover:bg-slate-400 active:bg-slate-400"
+              className="my-1 xs:my-2 xs:py-1  bg-white bg-opacity-50  rounded xs:mx-1 xs:text-xs focus-within:bg-slate-400 hover:bg-slate-400 active:bg-slate-400"
               onClick={() => setSelectedPage(SelectedPage.ShortBreak)}>
               <Link
                 page="ShortBreak"
@@ -99,7 +99,7 @@ const Hero: React.FC<Props> = ({
               />
             </button>
             <button
-              className="m-2 p-1  rounded xs:mx-1 xs:text-xs bg-white bg-opacity-50 focus-within:bg-slate-400 hover:bg-slate-400 active:bg-slate-400"
+              className="m-1 xs:m-2 xs:p-1  rounded xs:mx-1 xs:text-xs bg-white bg-opacity-50 focus-within:bg-slate-400 hover:bg-slate-400 active:bg-slate-400"
               onClick={() => setSelectedPage(SelectedPage.LongBreak)}>
               <Link
                 page="LongBreak"
