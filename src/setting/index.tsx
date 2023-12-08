8e102cbc0ab649c5588a556f1cbbd9fa8c86da61
PVSKR <skr.polu@hotmail.com>
2023-12-08 14:05:13 +0530

    accordion adding to setting

--- src/setting/index.tsx
+++ src/setting/index.tsx
@@ -1,4 +1,5 @@
 // Setting.tsx
+import React, { useEffect } from "react"; // Import React and useEffect
 import { SelectedPage } from "@/shared/types";
 import removesvg from "@/assets/remove.svg";
 import Timer from "@/Timer";
@@ -14,8 +15,18 @@ type Props = {
 };
 
 const Setting = ({ onClose }: Props) => {
+  useEffect(() => {
+    const accordions = document.querySelectorAll(".accordion");
+
+    accordions.forEach((accordion) => {
+      accordion.addEventListener("click", () => {
+        accordion.classList.toggle("max-h-[500px]");
+      });
+    });
+  }, []);
+
   return (
-    <div className="w-[24rm]  z-10 fixed p-4  m-2 mt-24 top-11 sm:mt-10 sm:w-[24rem] sm:right-[4rem] sm:fixed">
+    <div className="w-[24rm] z-10 fixed p-4 m-2 mt-24 top-11 sm:mt-10 sm:w-[24rem] sm:right-[4rem] sm:fixed">
       <div className="bg-white rounded shadow-lg">
         <div className="flex justify-between p-4 border-b-2 border-white-500">
           <h1 className="text-lg font-semibold">Setting</h1>
@@ -29,14 +40,14 @@ const Setting = ({ onClose }: Props) => {
             />
           </button>
         </div>
-        <div className="p-4 flex flex-col ">
-          <Timer />
-          <Task label={""} />
-
-          <Sound />
-
-          <Theme label={""} />
-          <Notifications />
+        <div className="p-4 flex flex-col">
+          <div className="max-h-0 overflow-hidden transition-max-h duration-300 ease-out">
+            <Timer />
+            <Task label={""} />
+            <Sound />
+            <Theme label={""} />
+            <Notifications />
+          </div>
         </div>
       </div>
     </div>
