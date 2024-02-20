6fb70df5ad22af32cb44789131f3ae056395c808
PVSKR <skr.polu@hotmail.com>
2024-02-20 23:47:15 +0530

    circle fill based on the percentage value.

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -4,33 +4,44 @@ type ProgressBarProps = {
   value: number; // Change the type to number for percentage
 };
 
-const ProgressBar: React.FC<ProgressBarProps> = ({ value }) => (
-  <div className="relative pt-1">
-    <div className="flex mb-2 items-center justify-center">
-      <div className="text-right ">
-        <span className="text-md font-semibold inline-block text-white ">
-          {value}%
-        </span>
-        <svg height="100" width="100">
-          <circle
-            r="45"
-            cx="50"
-            cy="50"
-            stroke="green"
-            strokeWidth="3"
-            fill="transparent"
-          />{" "}
-        </svg>
-      </div>
-    </div>
-    <div className="flex h-[.7rem]  mb-4 overflow-hidden text-xs bg-teal-500 rounded ">
-      <div
-        style={{ width: `${value}%` }}
-        className="flex flex-col justify-center  bg-blue-800 text-xs  text-center whitespace-nowrap text-white">
-        {value}%
-      </div>
-    </div>
-  </div>
-);
+const ProgressBar: React.FC<ProgressBarProps> = ({ value }) => {
+  const radius = 45;
+  const circumference = 2 * Math.PI * radius;
+  const strokeDasharray = `${circumference} ${circumference}`;
+  const strokeDashoffset = circumference - (value / 100) * circumference;
 
+  return (
+    <div className="relative pt-1">
+      <div className="flex mb-2 items-center justify-center">
+        <div className="text-right ">
+          <span className="text-md font-semibold inline-block text-white ">
+            {value}%
+          </span>
+          <svg height="100" width="100">
+            <circle
+              r={radius}
+              className="relative"
+              cx="50"
+              cy="50"
+              stroke="blue"
+              strokeWidth="3"
+              strokeDasharray={strokeDasharray}
+              strokeDashoffset={strokeDashoffset}>
+              <span className="absolute text-md font-semibold inline-block text-white ">
+                {value}%
+              </span>
+            </circle>
+          </svg>
+        </div>
+      </div>
+      <div className="flex h-[.7rem]  mb-4 overflow-hidden text-xs bg-teal-500 rounded ">
+        <div
+          style={{ width: `${value}%` }}
+          className="flex flex-col justify-center  bg-blue-800 text-xs  text-center whitespace-nowrap text-white">
+          {value}%
+        </div>
+      </div>
+    </div>
+  );
+};
 export default ProgressBar;
